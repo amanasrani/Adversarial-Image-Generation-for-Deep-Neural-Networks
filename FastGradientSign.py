6c24fa908eb7a76104434a1dd908586d89feac06
Hari Venugopalan <harivenugopalan@Haris-MacBook-Pro.local>
2017-11-12 18:39:13 -0800

    FGSM Implementation

---
+++ FastGradientSign.py
@@ -0,0 +1,11 @@
+from __future__ import division
+import numpy as np
+import scipy.io
+
+from TestMatFiles import X_Test, X_Train, Y_Test, Y_Train, Beta, Theta
+
+def fastGradient(H, O):
+	output_diff = (O - Y_Train)*O*(1-O)
+	hidden_layer_derivatives = np.dot(output_diff, Theta.T)*H*(1-H);
+	input_derivatives = np.dot(hidden_layer_derivatives, Beta.T)
+	return input_derivatives
