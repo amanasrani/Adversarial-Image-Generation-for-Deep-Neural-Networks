f5f996dd048239ea52d9d8b394791710f0f57fe0
Hari Venugopalan <harivenugopalan@Haris-MacBook-Pro.local>
2017-11-21 02:52:27 -0800

    Fixed bugs and added code to see accuracy on adversarial inputs

--- ATN.py
+++ ATN.py
@@ -1,9 +1,8 @@
 from __future__ import division
 import numpy as np
 import scipy.io
-
+import bigfloat
 from TestMatFiles import X_Test, X_Train, Y_Test, Y_Train, Beta, Theta
-from CNN_Output import Convolution, Subsampling, weightsMatrix, biasMatrix, propagate
 
 def l2_cost(x, y):
 	return (x-y)**2
@@ -22,38 +21,34 @@ def rerank(y, target):
 	return normalize(y[k], y)
 
 def sigmoid(z):
-    g = 1.0 /(1.0 + np.exp(-z))
+    g = 1.0 /(1.0 + bigfloat.exp(-z))
     return g
 
 def propagate(X):
     Z2 =  np.zeros((X.shape[0],Beta.shape[1]))
     H = np.zeros((X.shape[0],Beta.shape[1]))
-
     Z3 = np.zeros((X.shape[0], Theta.shape[1]))
     O = np.zeros((X.shape[0], Theta.shape[1]))
-
-	Z2 = np.dot(X, Beta)
-    H = sigmoid(z2)
-
+    Z2 = np.dot(X, Beta)
+    H = sigmoid(Z2)
     Z3 = np.dot(H, Theta)
     O = sigmoid(Z3)
-
     return O, H
 
 NumberOfClasses = 10
 NumberOfIterations = 1000
 
-Input_Layer_Size = X_Train.shape[1]
+
 Hidden_Layer_Size = 1024
 
 
 Bias_Ones = np.ones((X_Train.shape[0], 1))
 
 X_Train = np.column_stack((Bias_Ones, X_Train))
-
+Input_Layer_Size = X_Train.shape[1]
 Output_Layer_Size = Input_Layer_Size
 
-B = np.random.rand(Input_Layer_Size + 1, Hidden_Layer_Size)
+B = np.random.rand(Input_Layer_Size, Hidden_Layer_Size)
 T = np.random.rand(Hidden_Layer_Size, Output_Layer_Size)
 
 h = np.zeros((X_Train.shape[0],B.shape[1]))
@@ -67,18 +62,17 @@ atn_tuning_parameter = 0.2
 for p in range(NumberOfIterations):
 	h = np.dot(X_Train, B)
 	g = np.dot(h, T)
-	
 	# g is perturbed input
 	f, fh = propagate(g)
 	y, yh = propagate(X_Train)
 	# f is the prediction on the perturbed input.
 
-	cost = np.sum(l2_cost(g,X_Train) + l2_cost(f,rerank(y, target)))
+	#cost = np.sum(l2_cost(g,X_Train) + l2_cost(f,rerank(y, target)))
 
 	Lx_delta_output = 2*(g-X_Train)
 
-	Ly_output_diff = (f-y)*f*(1-f)
-	Ly_hidden_layer_derivatives = np.dot(Ly_output_diff, Theta.T)*fh*(1-fh)/
+	Ly_output_diff = (f-rerank(y, target))*f*(1-f)
+	Ly_hidden_layer_derivatives = np.dot(Ly_output_diff, Theta.T)*fh*(1-fh)
 	Ly_delta_output = np.dot(Ly_hidden_layer_derivatives, Beta.T)
 
 	delta_output = Lx_delta_output + Ly_delta_output
@@ -90,5 +84,17 @@ for p in range(NumberOfIterations):
 	B = B + learning_rate * (np.dot(delta_Hidden.T, X_Train)/X_Train.shape[0]).T
 
 
+hidden_perturbed = np.dot(X_Train, B)
+output_perturbed = np.dot(hidden_perturbed, T)
 
+X_Perturbed = np.tanh(X_Train+output_perturbed)
 
+Y_Perturbed = propagate(X_Perturbed)
+
+I = np.argmax(Y_Perturbed, axis= 1)
+I_ = np.argmax(Y_Train, axis=1)
+
+A = (I == I_)
+NumberOfCorrectSamples = np.sum(A)
+TrainingError[p] = 1 - (NumberOfCorrectSamples/X_Train.shape[0])
+print(p)
--- FastGradientSign.py
+++ FastGradientSign.py
@@ -6,6 +6,6 @@ from TestMatFiles import X_Test, X_Train, Y_Test, Y_Train, Beta, Theta
 
 def fastGradient(H, O):
 	output_diff = (O - Y_Train)*O*(1-O)
-	hidden_layer_derivatives = np.dot(output_diff, Theta.T)*H*(1-H);
+	hidden_layer_derivatives = np.dot(output_diff, Theta.T)*H*(1-H)
 	input_derivatives = np.dot(hidden_layer_derivatives, Beta.T)
 	return input_derivatives
